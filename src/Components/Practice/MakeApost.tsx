acb4b9dc8803d527657661882c6808e9fe660a8a
nicsylvia <nicsylvia15f@gmail.com>
2023-02-21 14:01:02 -0800

    saved

--- src/Components/Practice/MakeApost.tsx
+++ src/Components/Practice/MakeApost.tsx
@@ -5,7 +5,15 @@ const Form = () => {
   return (
     <div>
         <Container>
-
+        <input type="text" placeholder='Enter post tittle' />
+        <br />
+        <br />
+        <input type="text" placeholder='Describe your post' />
+        <br /><br />
+        <button>Upload Post</button>
+        <br />
+        <br />
+        <Card></Card>
         </Container>
     </div>
   )
@@ -15,9 +23,27 @@ export default Form;
 
 const Container = styled.div`
   width: 100%;
+  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-wrap: wrap;
-  background-color: #afaf46;
+  background-color: black;
+  flex-direction: column;
+  input{
+    width: 300px;
+    height: 30px;
+  }
+  button{
+    width: 200px;
+    height: 40px;
+    border-radius: 10px;
+    background-color: purple;
+    outline: purple;
+  }
+`;
+const Card = styled.div`
+  width: 400px;
+  background-color: purple;
+  padding: 10px;
 `;
