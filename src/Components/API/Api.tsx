7762ceade993e965b7c84b521822e3a7f6e1f46d
nicsylvia <nicsylvia15f@gmail.com>
2023-02-21 14:26:07 -0800

    saved

--- src/Components/API/Api.tsx
+++ src/Components/API/Api.tsx
@@ -3,5 +3,7 @@ import axios from "axios";
 const Endpoint = "http://localhost:3400/api";
 
 export const GetAll = async() =>{
-    return axios.get(`${Endpoint}`)
+    return axios.get(`${Endpoint}/post/getposts`).then((res) =>{
+        res.data
+    })
 }
